75573bd9727ba529133340b04cfaa392b4b43e9f
Vrinda Vaish <vrinda1291@gmail.com>
2013-03-08 13:58:54 -0700

    Updated UI Code

--- PhotoShells.singleView/PhotoShells.singleView/ViewController.m
+++ PhotoShells.singleView/PhotoShells.singleView/ViewController.m
@@ -34,4 +34,16 @@
     extractedText = [ocr extractText:imgArray];
 }
 
+- (IBAction)imageProcessing:(id)sender {
+}
+
+- (IBAction)folderSettings:(id)sender {
+}
+
+- (IBAction)categorisationLogs:(id)sender {
+}
+
+- (IBAction)appSwitch:(id)sender {
+}
+
 @end
